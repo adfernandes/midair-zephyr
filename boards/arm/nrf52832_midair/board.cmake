ba8ccab2aaa812af4a08ca036d61826ec51245eb
Andrew Fernandes <andrew@fernandes.org>
2019-11-02 14:34:04 -0700

    Tidy and clean up the project a little

--- boards/arm/nrf52832_midair/board.cmake
+++ boards/arm/nrf52832_midair/board.cmake
@@ -4,9 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+set(JLINK_SERIAL_NUMBER "260114307")
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--erase" "--snr" "${JLINK_SERIAL_NUMBER}")
+board_runner_args(jlink "--device=nrf52" "--snr=${JLINK_SERIAL_NUMBER}")
 board_runner_args(pyocd "--target=nrf52832")
-board_runner_args(nrfjprog "--nrf-family=NRF52")
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
