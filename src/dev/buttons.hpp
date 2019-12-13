98c3d484d9ce9e0ecc1f3556ff7906e0713bd6f8
Andrew Fernandes <andrew@fernandes.org>
2019-12-12 20:13:28 -0800

    Clang-tidy: use trailing return type

--- src/dev/buttons.hpp
+++ src/dev/buttons.hpp
@@ -13,15 +13,15 @@ enum class btn_pin : u32_t {
 extern atomic_t red_btn_state;
 extern atomic_t grn_btn_state;
 
-static inline bool get_red_btn_state(void) {
+static inline auto get_red_btn_state(void) -> bool {
     return bool(atomic_get(&red_btn_state));
 }
 
-static inline bool get_grn_btn_state(void) {
+static inline auto get_grn_btn_state(void) -> bool {
     return bool(atomic_get(&grn_btn_state));
 }
 
-static inline bool get_btn_state(const btn_pin pin) {
+static inline auto get_btn_state(const btn_pin pin) -> bool {
     switch (pin) {
         case btn_pin::red: return get_red_btn_state();
         case btn_pin::grn: return get_grn_btn_state();
--- src/dev/lsm6dsox.hpp
+++ src/dev/lsm6dsox.hpp
@@ -9,9 +9,9 @@ void configure_lsm6dsox(void);
 
 const u8_t spi_orc = CONFIG_SPI_0_NRF_ORC;
 
-static inline u8_t spi_write_register(const u8_t r ) { return r; }
-static inline u8_t spi_read_register (const u8_t r ) { return UINT8_C(0x80) | r; }
+static inline auto spi_write_register(const u8_t r ) -> u8_t { return r; }
+static inline auto spi_read_register (const u8_t r ) -> u8_t { return UINT8_C(0x80) | r; }
 
-int spi_simple_transceive(struct device *dev, const struct spi_config *config, u8_t *tx_buffer, u8_t *rx_buffer, size_t length);
+auto spi_simple_transceive(struct device *dev, const struct spi_config *config, u8_t *tx_buffer, u8_t *rx_buffer, size_t length) -> int;
 
 //----------------------------------------------------------------------
