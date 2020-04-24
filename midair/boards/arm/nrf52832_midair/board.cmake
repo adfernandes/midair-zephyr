0f29fa94ca03a9368d2cc5132253fcf17898d031
Andrew Fernandes <andrew@fernandes.org>
2020-04-24 14:50:13 -0700

    Full project generation and ozone debugging... hooray!

--- midair/boards/arm/nrf52832_midair/board.cmake
+++ midair/boards/arm/nrf52832_midair/board.cmake
@@ -3,12 +3,10 @@
 # Copyright (c) 2020 Andrew Fernandes <andrew@fernandes.org>
 # SPDX-License-Identifier: Apache-2.0
 
-#|set(JLINK_SERIAL_NUMBER "260114307")
-#|
-#|board_runner_args(nrfjprog "--nrf-family=NRF52" "--erase" "--snr" "${JLINK_SERIAL_NUMBER}")
-#|board_runner_args(jlink "--device=nrf52" "--snr=${JLINK_SERIAL_NUMBER}")
-#|board_runner_args(pyocd "--target=nrf52832")
-#|
-#|include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-#|include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-#|include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+board_runner_args(nrfjprog "--nrf-family=NRF52") # "--erase" "--snr" "${JLINK_SERIAL_NUMBER}"
+board_runner_args(jlink "--device=nrf52" "--speed=8000") # "--snr=${JLINK_SERIAL_NUMBER}"
+board_runner_args(pyocd "--target=nrf52832" "--frequency=8000000")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
